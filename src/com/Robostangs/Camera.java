ee742376eef79ba598d0d70664f44f3a6b16d85c
sky <sky@robostangs.com>
2014-04-09 14:43:01 -0400

    added saving image to crio

--- src/com/Robostangs/Camera.java
+++ src/com/Robostangs/Camera.java
@@ -6,6 +6,11 @@
 
 package com.Robostangs;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.camera.AxisCamera;
+import edu.wpi.first.wpilibj.camera.AxisCameraException;
+import edu.wpi.first.wpilibj.image.ColorImage;
+import edu.wpi.first.wpilibj.image.NIVisionException;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 /**
@@ -15,9 +20,11 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 public class Camera {
     private static NetworkTable table = null;
     private static Camera _instance = null;
+    private static AxisCamera cam;
         	
     private Camera() {
     	table = NetworkTable.getTable("camera");
+        cam = AxisCamera.getInstance("10.5.48.11");
     }
     
     public static Camera getInstance() {
@@ -44,6 +51,24 @@ public class Camera {
      */
     public static boolean hot() {
 	return table.getBoolean("hot", false);
+    }
+    
+    /**
+     * Saves an image to the crio
+     */
+    public static void saveImage() {
+        if (cam.freshImage()) {
+            ColorImage image = null;
+            try {
+                image = cam.getImage();
+                image.write("../../images/orig" + Timer.getFPGATimestamp() + ".jpg");
+                image.free();
+            } catch (AxisCameraException ex) {
+                ex.printStackTrace();
+            } catch (NIVisionException ex) {
+                ex.printStackTrace();
+            }        
+        }
     }
 
 }
